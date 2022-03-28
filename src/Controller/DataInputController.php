d1577a1bd73e8d94401681efb35705f83f211f01
GijsvanGemert <Gijsvangemert@live.nl>
2022-03-28 14:29:00 +0200

    last changes for the branch

--- src/Controller/DataInputController.php
+++ src/Controller/DataInputController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ReisgegevensRepository;
 use App\Entity\Reisgegevens;
 use App\Form\ReisgegevensType;
 use Doctrine\Persistence\ManagerRegistry;
@@ -22,10 +23,13 @@ class DataInputController extends AbstractController
     }
 
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ReisgegevensRepository $rg): Response
     {
+
+        $reisgegevens = $rg->MaxDistanceBiked(intval(date("m",strtotime("-1 month"))),intval(date("Y",strtotime("-1 month"))));
         return $this->render('data_input/index.html.twig', [
             'controller_name' => 'DataInputController',
+            'reisgegevens' => $reisgegevens
         ]);
     }
 
